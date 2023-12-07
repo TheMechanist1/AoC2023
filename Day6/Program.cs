fd69503eae6f7084605b513feaa9b7685a1aab7d
Mechanist <dylanjcooksley@gmail.com>
2023-12-07 00:24:18 -0600

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    //Time:        45     97     72     95
+    //Distance:   305   1062   1110   1695
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long time = 45977295;
+            long dist = 305106211101695;
+
+            var mult = 0;
+            for(int i = 0; i < time; i++)
+            {
+                var ways = i * (time - i) > dist;
+                if (ways)
+                {
+                    mult += 1;
+                }
+            }
+
+            Console.WriteLine(mult);
+        }
+    }
+}
+
+//28 * 72 * 27 * 48
